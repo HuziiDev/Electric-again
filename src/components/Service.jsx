a43de364446072196c6e59d0d3113ff7f7c6f9eb
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-03-28 00:34:23 +0530

    fd

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -5,28 +5,28 @@ import { FaGift } from "react-icons/fa6";
 import { FaHeadphonesSimple } from "react-icons/fa6";
 const Service = () => {
   return (
-    <div className='w-[80%] flex justify-between items-center  h-auto mx-auto mt-6 mb-10'>
+    <div className='service-container w-[80%] flex justify-between items-center  h-auto mx-auto mt-6 mb-10'>
         <div className="secure-payment "> 
         <span > <RiSecurePaymentLine className='text-yellow-500 ' size={30} /> </span>
-        <span className='text-black font-semibold '>Secure Payment</span>
+        <span className='text-black font-semibold md:text-lg sm:text-sm  '>Secure Payment</span>
         
             
         </div>
         <div className="online-support "> 
         <span > <FaHeadphonesSimple className='text-yellow-500 ' size={30} /> </span>
-        <span className='text-black font-semibold '>Online Support</span>
+        <span className='text-black font-semibold md:text-lg sm:text-sm '>Online Support</span>
         
             
         </div>
         <div className="shipping "> 
         <span > <MdLocalShipping className='text-yellow-500 ' size={30} /> </span>
-        <span className='text-black font-semibold '>Free Shipping</span>
+        <span className='text-black font-semibold md:text-lg sm:text-sm '>Free Shipping</span>
         
             
         </div>
         <div className="discount "> 
         <span > <FaGift  className='text-yellow-500 ' size={30} /> </span>
-        <span className='text-black font-semibold '>Discount Online</span>
+        <span className='text-black font-semibold md:text-lg sm:text-sm '>Discount Online</span>
         
             
         </div>
