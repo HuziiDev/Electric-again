8b07a1b0d3b5a8eb766c3341dad6801d1799ef2f
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-03-29 00:37:10 +0530

    fdf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 import img from '../assets/bulb.png'
 
 const Hero = () => {
   return (
     <div className='hero-container w-[100%] h-auto bg-blue-200 flex justify-around items-center flex-wrap '>
 
-         <div className="hero-left pt-10">
+         <motion.div className="hero-left pt-10"
+         initial={{opacity:0,x:-100}}
+         animate={{opacity:1,x:0}}
+         exit={{opacity:1,x:0}}
+         transition={{ease:'easeInOut', duration:1}}
+         
+         >
             <p className='text-lg text-black font-medium pb-4 '>Online supply of essentials</p>
             <h1 className='text-5xl font-bold pb-2'>Get Your Electric</h1>
             <h1 className='text-5xl font-bold pb-6'>Appliance at Home</h1>
             <button className='px-8 py-2 bg-yellow-500 rounded-full hover:scale-125 duration-200 font-semibold'>Shop Now</button>
-         </div>
+         </motion.div>
 
 
-         <div className="hero-right pt-16">
+         <motion.div className="hero-right pt-16"
+          initial={{opacity:0,x:100}}
+         animate={{opacity:1,x:0}}
+         exit={{opacity:1,x:0}}
+         transition={{ease:'easeInOut', duration:1}}
+         >
          <img className='object-cover' src={img} alt="" />
 
-         </div>
+         </motion.div>
 
     </div>
   )
--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import img from '../assets/bulb-incline.png'
+import { motion } from 'framer-motion'
+
 
 const ProductDisplay = () => {
+  const variants = {
+    initial: {
+      opacity: 0,
+      y: 30
+    },
+    animate: {
+      opacity: 1,
+      y: 0
+    }
+  }
+ 
   return (
-    <div className='w-[90%] h-auto flex flex-wrap justify-evenly items-center mt-10 mx-auto'>
-        <div className="left-display flex rounded-2xl bg-green-100 mt-10 mb-10" >
+    <div className='w-[90%] h-auto flex flex-wrap justify-evenly items-center  mx-auto'>
+        <motion.div className="left-display flex rounded-2xl bg-green-100 mt-10 mb-10"
+       >
         <div className="content p-10">
           <p className='text-gray-800 font-semibold'>Starting with ₹100</p>
           <p className='text-gray-800 font-semibold'>Your daily Essentials</p>
           <h1 className='text-3xl font-bold mt-4'>Emergency Bulb</h1>
-          <button className='px-5 py-1 bg-yellow-500 mt-4 rounded-full '>Shop Now</button>
+          <motion.button className='px-5 py-1 bg-yellow-500 mt-4 rounded-full font-semibold '
+          variants={variants}
+          initial="initial"
+          whileInView="animate"
+          viewport={{once:true}}
+          transition={{ease:'easeInOut', duration:1}}
+         
+          >
+          Shop Now</motion.button>
         </div>
         <div className="img w-44 h-20 pt-28">
         <img src={img} alt="" />
@@ -17,20 +39,26 @@ const ProductDisplay = () => {
         </div>
 
 
-        </div>
-        <div className="right-display flex rounded-2xl bg-gray-300 mt-10 mb-10 ">
+        </motion.div>
+        <motion.div className="right-display flex rounded-2xl bg-gray-300 mt-10  mb-10 "  >
         <div className="content p-10">
           <p className='text-gray-800'>Starting with ₹100</p>
           <p className='text-gray-800'>Your daily Essentials</p>
           <h1 className='text-3xl font-bold mt-4'>Emergency Bulb</h1>
-          <button className='px-5 py-1 bg-yellow-500 mt-4 rounded-full'>Shop Now</button>
+          <motion.button className='px-5 py-1 bg-yellow-500 mt-4 rounded-full font-semibold'
+           variants={variants}
+          initial="initial"
+          whileInView="animate"
+          viewport={{once:true}}
+          transition={{ease:'easeInOut', duration:1}}
+          >Shop Now</motion.button>
         </div>
         <div className="img w-44 h-20 pt-28">
         <img src={img} alt="" />
 
         </div>
             
-        </div>
+        </motion.div>
     </div>
   )
 }
